68ea4fc2d8df2f4526b22939b51a586278a3a322
daniel-web-developer <daniel_caixeta@protonmail.com>
2023-08-29 16:43:50 +0200

    sort working (except with search)

--- blogwebsite/static/script.js
+++ blogwebsite/static/script.js
@@ -1,5 +1,6 @@
 const articles = document.querySelector('#articles');
 const search = document.querySelector('#search');
+const select = document.querySelector('#select');
 
 const urlAll = 'http://localhost:8000/api/articles/?format=json';
 
@@ -11,38 +12,56 @@ function fetchAll(){
     fetch(urlAll)
         .then(response => response.json())
         .then(data => {
-            renderData(data);
+            organiseData(data);
         });
 }
 
-function renderData(apiData){
+function organiseData(apiData){
 
-    renderArticles = apiData.map((article) => 
+    let data = apiData;
+
+    search.addEventListener("keyup", (event) => {
+        console.log(select.value);
+        searchValue = event.target.value;
+        searchValue = searchValue.toLowerCase();
+        data = apiData.filter(article => article.title.toLowerCase().includes(searchValue))
+        sortData(data, select.value);
+    })
+    sortData(data, select.value);
+}
+
+function sortData(data, value){
+    console.log(value);
+
+    let sortedData = null;
+
+    if (value == 1){
+        sortedData = data.sort((a, b) => a.date_posted - b.date_posted ? -1 : 1);
+    }
+    else if (value == 2){
+        sortedData = data.sort((a, b) => a.date_posted - b.date_posted ? 1 : -1);
+    }
+    else if (value == 3){
+        sortedData = data.sort((a, b) => a.title - b.title ? -1 : 1);
+    }
+    else if (value == 4){
+        sortedData = data.sort((a, b) => a.title - b.title ? 1 : -1);
+    }
+
+    renderData(sortedData);
+}
+
+function renderData(data){
+    renderArticles = data.map((article) => 
         `
         <div>
-        <a href="{% url 'article' article.permalink %}" class="all-title">${article.title}</a>
+        <a href="/article/${article.permalink}" class="all-title">${article.title}</a>
         <p class="all-body">${article.content}</p>
         </div>
         `
     );
-    articles.innerHTML = renderArticles;
-
-    search.addEventListener("keydown", (event) => {
-        searchValue = event.target.value;
-        searchValue = searchValue.toLowerCase();
-        data = apiData.filter(article => article.title.toLowerCase().includes(searchValue))
-        renderArticles = data.map((article) => 
-            `
-            <div>
-            <a href="{% url 'article' article.permalink %}" class="all-title">${article.title}</a>
-            <p class="all-body">${article.content}</p>
-            </div>
-            `
-        );
-        articles.innerHTML = renderArticles;
-    })
-
-    }
+    articles.innerHTML = renderArticles.join('');
+}
 
 fetchAll();
 
